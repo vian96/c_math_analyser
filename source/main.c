af8717f7ef1427ac7bbfb45fc1c9610ffdc2b6ab
vian96 <51120611+vian96@users.noreply.github.com>
2021-08-16 17:47:11 +0300

    Algorithm is a little changed, added basic simplification

--- source/main.c
+++ source/main.c
@@ -2,18 +2,15 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <math.h>
-
 #include <assert.h>
 
-int glob_id = 0; // for debugging
 
 enum expr_type{
     EXPR_CONST, EXPR_VAR, EXPR_MULT,
-    EXPR_SUM, //EXPR_DIV, EXPR_EXP
+    EXPR_SUM,
 };
 
 struct math_tree{
-    int id;
     enum expr_type expr;
     struct math_tree *parts;
     double data;
@@ -33,11 +30,11 @@ int count_factors(const char *s);
 int count_terms(const char *s);
 
 // read functions
-struct math_tree read_const(const char *s, char **p);
+struct math_tree read_const(const char *s, char **ptr);
 
-struct math_tree read_var(const char *s, char **p);
+struct math_tree read_var(const char *s, char **ptr);
 
-struct math_tree read_factor(const char *s, char **p);
+struct math_tree read_factor(const char *s, char **ptr);
 
 struct math_tree read_mult(const char *s, char **ptr);
 
@@ -48,6 +45,13 @@ void print_math_tree(struct math_tree tree);
 
 void print_tree_data(struct math_tree tree);
 
+// tree analysis functions
+struct math_tree make_const_var(double num, double power);
+
+void simplify_tree(struct math_tree *tree);
+
+void free_math_tree(struct math_tree tree);
+
 
 // -----------------------------------------------
 // main function
@@ -56,22 +60,31 @@ void print_tree_data(struct math_tree tree);
 int main(void) {
     struct math_tree tree;
     char *s[]={
-        "x+2", "3*x^2-2*x+3","(x+2)+1",
-        "(x-2)*(x-3)*(2*x^2-3)-37*(x^2-3*x^5)",
-        "(37*x^3-42*(43-8)+x+32*x)*(657*(657*879*x^2))+x^3*3"
+            "x+2", "3*x^2-2*x+3","(x+2)+1",
+            "(x-2)*(x-3)*(2*x^2-3)-37*(x^2-3*x^5)",
+            "(37*x^3-42*(43-8)+x+32*x)*(657*(657*879*x^2))+x^3*3"
     };
     for (int i=0; i<sizeof(s)/sizeof(*s);i++){
         tree = read_sum(s[i], NULL);
         print_math_tree(tree);
+        printf("\n");
+
+        simplify_tree(&tree);
+        print_math_tree(tree);
         printf("\n\n");
+        
+        free_math_tree(tree);
     }
     return 0;
 }
 
 
 // -----------------------------------------------
-// functions' iplementations
+// functions' implementations
 // -----------------------------------------------
+
+
+// string analysis functions
 
 const char* skip_bracket_expr(const char *s){
     // returns pointer to ')'
@@ -83,69 +96,62 @@ const char* skip_bracket_expr(const char *s){
         else if (*s==')')
             brackets--;
     }
-    assert(brackets==0 || "ERROR wrong brackets");
+    assert(("ERROR: wrong brackets", brackets==0));
     return s;
 }
 
 int count_factors(const char *s){
     int factors = 1;
-    
-    // "-x" should be equal to "(-1)*x"
-    if (s[0]=='-' && !isdigit(s[1])) factors++; 
-    if (*s=='+' || *s=='-') s++;
 
-    for (; *s!=0 && *s!=')' && *s!='+' && *s!='-'; s++) {
+    // "-x" should be equal to "(-1)*x"
+    if (s[0]=='-' && !isdigit(s[1])) 
+        factors++;
+    if (*s=='+' || *s=='-') 
+        s++;
+
+    for (; *s!=0 && *s!=')' && *s!='+' && *s!='-'; s++)
         if (*s=='(')
             s = skip_bracket_expr(s);
         else
             factors += (*s=='*');
-    }
     return factors;
 }
 
 int count_terms(const char *s){
     int cnt = (*s!='-' && *s!='+');
-    for (; *s && *s!=')'; s++) {
-        if (*s=='('){
+    for (; *s && *s!=')'; s++) 
+        if (*s=='(')
             s = skip_bracket_expr(s);
-        }
         else
             cnt += (*s=='-' || *s=='+');
-    }
     return cnt;
 }
 
-struct math_tree read_const(const char *s, char **p){
-    return (struct math_tree){
-        .expr = EXPR_CONST, .parts = NULL, 
-        .tree_len = 0, .data = strtof(s, p), 
-        .id=glob_id++
-    };
+
+// read functions
+
+struct math_tree read_const(const char *s, char **ptr){
+    return make_const_var(strtof(s, ptr), 0);
 }
 
-struct math_tree read_var(const char *s, char **p){
+struct math_tree read_var(const char *s, char **ptr){
     // name doesn't matter, it is skipped
-    // data is power of var: "x^256" - data is 256
+    // data is power of var: "abc^256" - data is 256
     while (isalnum(*++s)) ;
-    *p = (char*)s;
-    return (struct math_tree){
-        .expr = EXPR_VAR, .parts = NULL, 
-        .tree_len = 0,
-        .data = (*s=='^') ? strtof(s+1, p) : 1,
-        .id=glob_id++
-    }; 
+    *ptr = (char*)s;
+    return make_const_var(1, (*s=='^') ? strtof(s+1, ptr) : 1);
 }
 
-struct math_tree read_factor(const char *s, char **p){
+struct math_tree read_factor(const char *s, char **ptr){
     if (*s=='('){
-        struct math_tree temp = read_sum(s+1, p);
-        (*p)++; // to avoid pointing to ')'
+        struct math_tree temp = read_sum(s+1, ptr);
+        (*ptr)++; // to avoid pointing to ')'
         return temp;
     }
-    if (s[0]=='-' || isdigit(s[0])) 
-        return read_const(s, p);
+    if (s[0]=='-' || isdigit(s[0]))
+        return read_const(s, ptr);
     if (isalpha(s[0]))
-        return read_var(s, p);
+        return read_var(s, ptr);
 
     printf("ERROR reading factor:\n%s\n", s);
     assert(0);
@@ -154,32 +160,28 @@ struct math_tree read_factor(const char *s, char **p){
 struct math_tree read_mult(const char *s, char **ptr){
     struct math_tree tree;
     int factors = count_factors(s);
-    
+
     if (factors>1){
         tree = (struct math_tree){
             .tree_len = factors, .expr = EXPR_MULT,
             .parts = calloc(factors, sizeof(tree)),
-            .id=glob_id++,
         };
-        
+
         int i=0;
-        // "-x" becomes (-1)*x
+        // "-x" becomes "(-1)*x"
         if (s[0]=='-' && !isdigit(s[1])) {
-            tree.parts[0] = (struct math_tree){
-                .expr = EXPR_CONST, .parts = NULL, 
-                .tree_len = 0, .data = -1
-            };
+            tree.parts[0] = make_const_var(-1, 0);
             s++; i++;
         }
-        
+
         char *p = (char*)s;
-        for(; i<factors; i++, p++){
+        for (; i<factors; i++, p++)
             tree.parts[i] = read_factor(p, &p);
-        }
+
         if (ptr)
-            *ptr = p-1; 
+            *ptr = p-1;
     }
-    else{
+    else {
         tree = read_factor(s, ptr);
     }
     return tree;
@@ -187,23 +189,22 @@ struct math_tree read_mult(const char *s, char **ptr){
 
 struct math_tree read_sum(const char *s, char **ptr){
     struct math_tree tree;
-    int terms = count_terms(s); 
-    
+    int terms = count_terms(s);
+
     if (terms>1) {
         tree = (struct math_tree){
             .expr = EXPR_SUM, .tree_len = terms,
             .parts = calloc(terms, sizeof(tree)),
-            .id=glob_id++
         };
-        
+
         int i=0;
         char *p = (char*)s;
-        for(; i<terms; i++){
+        for (; i<terms; i++){
             tree.parts[i] = read_mult(p, &p);
-            if(*p=='+') 
+            if (*p=='+')
                 p++;
         }
-        if(ptr) 
+        if (ptr)
             *ptr = p;
     }
     else {
@@ -212,25 +213,28 @@ struct math_tree read_sum(const char *s, char **ptr){
     return tree;
 }
 
+
+// print functions
+
 void print_math_tree(struct math_tree tree){
     if (tree.expr == EXPR_SUM){
-        for(int i=0; i<tree.tree_len; i++){
+        printf("(");
+        for (int i=0; i<tree.tree_len; i++){
             print_math_tree(tree.parts[i]);
-            printf((i+1!=tree.tree_len)?"+":"");
+            printf((i+1!=tree.tree_len) ? "+" : "");
         }
+        printf(")");
     }
     else if (tree.expr == EXPR_MULT){
-        for(int i=0; i<tree.tree_len; i++){
-            if (tree.parts[i].expr==EXPR_SUM)
-                printf("(");
+        printf("(");
+        for (int i=0; i<tree.tree_len; i++){
             print_math_tree(tree.parts[i]);
-            if (tree.parts[i].expr==EXPR_SUM)
-                printf(")");
-            printf((i+1!=tree.tree_len)?"*":"");
+            printf((i+1!=tree.tree_len) ? "*" : "");
         }
+        printf(")");
     }
     else if (tree.expr == EXPR_CONST){
-        printf(tree.data>0?"%.0f":"(%.0f)", tree.data);
+        printf(tree.data>0 ? "%.0f" : "(%.0f)", tree.data);
     }
     else if (tree.expr == EXPR_VAR){
         if (fabs(tree.data-1)>0.001)
@@ -242,10 +246,78 @@ void print_math_tree(struct math_tree tree){
 
 void print_tree_data(struct math_tree tree){
     printf(
-        "%d %d %d %.0f\n",
-        tree.id, tree.expr, tree.tree_len, tree.data
-    );
-    for(int i=0; i<tree.tree_len; i++){
+            "%d %d %.0f\n",
+            tree.expr, tree.tree_len, tree.data
+            );
+    for (int i=0; i<tree.tree_len; i++)
         print_tree_data(tree.parts[i]);
+}
+
+
+// tree analysis functions
+
+struct math_tree make_const_var(double num, double power){
+    struct math_tree tree = {
+        .expr = EXPR_MULT, .data = 0
+    };
+    // TODO float comparison
+    if (power && num!=1){
+        tree.parts = malloc(sizeof(tree)*2);
+        tree.tree_len = 2;
+        tree.parts[0] = (struct math_tree){
+            .expr = EXPR_CONST, .parts = 0,
+            .data = num, .tree_len = 0
+        };
+        tree.parts[1] = (struct math_tree){
+            .expr = EXPR_VAR, .parts = 0,
+            .data = power, .tree_len = 0
+        };
     }
+    else if (power)
+        tree = (struct math_tree){
+            .expr = EXPR_VAR, .parts = 0,
+            .data = power, .tree_len = 0
+        };
+    else
+        tree = (struct math_tree){
+            .expr = EXPR_CONST, .parts = 0,
+            .data = num, .tree_len = 0
+        };
+    return tree;
+}
+
+void simplify_tree(struct math_tree *tree){
+    for (int i=0; i<tree->tree_len; i++)
+        simplify_tree(tree->parts+i);
+    if (tree->expr == EXPR_SUM || tree->expr == EXPR_MULT){
+        // (x+1)+2 => x+1+2
+        int new_len = tree->tree_len;
+        for (int i=0; i<tree->tree_len; i++)
+            if (tree->parts[i].expr == tree->expr)
+                new_len += tree->parts[i].tree_len - 1;
+
+        if (new_len != tree->tree_len){
+            tree->parts = realloc(
+                tree->parts, sizeof(*tree)*new_len
+            );
+
+            // TODO make it more beautiful
+            for (int i=0, n=tree->tree_len; i<tree->tree_len; i++)
+                if (tree->parts[i].expr == tree->expr){
+                    for (int k=1; k < tree->parts[i].tree_len; k++)
+                        tree->parts[n++] = tree->parts[i].parts[k];
+                    void *t=tree->parts[i].parts;
+                    tree->parts[i] = tree->parts[i].parts[0];
+                    free(t);
+                }
+
+            tree->tree_len = new_len;
+        }
+    }
+}
+
+void free_math_tree(struct math_tree tree){
+    for (int i=0; i<tree.tree_len; i++)
+        free_math_tree(tree.parts[i]);
+    free(tree.parts);
 }
